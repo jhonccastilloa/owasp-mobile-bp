95d0f16939f22879d8d32f90e3a800bb06814894
Jhon Castillo <gpro1pro@gmail.com>
2025-04-02 14:03:39 -0500

    feat(rasp): rasp detection config

--- src/platform/android/javaLogs/javaLogsAnalyzer.ts
+++ src/platform/android/javaLogs/javaLogsAnalyzer.ts
@@ -3,11 +3,30 @@ import searchJavaLogs from './searchJavaLogs';
 import verifyProguardConfig from './verifyProguardConfig';
 import { PermissionData } from '@/types/global';
 import { PermissionStatus } from '@/types/enums';
+import fs from 'fs';
 
 const javaLogsAnalyze = async (currentPath: string) => {
   const androidPath = path.join(currentPath, 'android');
+  const buildGradleAppPath = path.join(
+    currentPath,
+    'android',
+    'app',
+    'build.gradle'
+  );
+  const regexRasp =
+    /implementation\s*\(\s*name:\s*'ShieldSDK',\s*ext:\s*'aar'\s*\)/;
+  const buildGradleAppContent = fs.readFileSync(buildGradleAppPath, 'utf-8');
   const javaLogsFound = await searchJavaLogs(androidPath);
-  const { status, message } = verifyProguardConfig(currentPath, {});
+  let message = '';
+  let status = false;
+  if (regexRasp.test(buildGradleAppContent)) {
+    status = true;
+    message = 'No se encontraron logs en archivos Java.';
+  } else {
+    const proguard = verifyProguardConfig(currentPath, {});
+    status = proguard.status;
+    message = proguard.message;
+  }
 
   const data: PermissionData = {
     numLine: null,

ada2285b969fbfd964d3715bfe961d52cbbf3e92
Hilary <hbeltran@dlya.com.uy>
2025-04-08 16:14:51 -0500

    command documentation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { automate, verify } from './commands';
+const { exec } = require('child_process');
 
 const main = async () => {
   const args = process.argv.slice(2);
@@ -10,6 +11,20 @@ const main = async () => {
     case 'automate':
       automate(currentPath);
       break;
+
+      case 'documentation':
+        exec('start https://github.com/jhonccastilloa/owasp-mobile-bp/blob/main/DOCUMENTATION.md', (err:any, stdout:any, stderr:any) => {
+          if (err) {
+            console.error(`Error: ${err}`);
+            return;
+          }
+          if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+          }
+          console.log(`stdout: ${stdout}`);
+        });
+        break;
     default:
       console.log(
         "Comando no reconocido. Usa 'owasp -verify' para verificar el proyecto."
